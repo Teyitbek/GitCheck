3411e389bf73244188dd2f7d344013566f22c43b
Teyit <ajimamatovteyitbek@gmail.com>
2023-02-11 15:15:40 +0600

    Git init

---
+++ GitCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitCheck
+//
+//  Created by Tiku on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
